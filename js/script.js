edc495df1f1da9be51965be2890c01f318d5354c
Tayo <tayoalabi@hotmail.co.uk>
2019-12-11 11:09:10 +0000

    edited background

--- js/script.js
+++ js/script.js
@@ -140,3 +140,12 @@ window.addEventListener("resize",function(){
 })
 
 init();
+
+
+
+//Tayo pictures//
+
+// MDB Lightbox Init
+$(function () {
+ $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
